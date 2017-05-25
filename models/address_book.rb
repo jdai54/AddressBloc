1db19004161509511386a3e233ec032830c70b8d
Jake <jdai54@gmail.com>
2017-05-24 17:16:22 -0700

    Assignment 4 Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,4 +21,19 @@ class AddressBook
     # #11 insert a new entry into entries using the calculated index
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  #def remove_entry(name, phone_number, email)
+#    entries.each do |entry|
+#      entries.delete_at(entries.index(entry.name)) if name == entry.name
+#    end
+#  end
+
+  def remove_entry(name, phone_number, email)
+   @entries.each_with_index do |value, index|
+      if value.name == name && value.phone_number == phone_number && value.email == email
+        @entries.delete_at(index)
+        break
+      end
+    end
+  end
 end
